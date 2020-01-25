51a22d616a19e63a7b00b78983d6103779a74078
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-25 13:42:07 -0500

    made port assignment dynamic

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
