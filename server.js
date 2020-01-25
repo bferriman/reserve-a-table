faf5dd7665b5b737a6576e93c407ac346a746f36
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-25 13:38:14 -0500

    built html pages and implemented server

---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app data
+
+const tables = [];
+
+const waitlist = [];
+
+//routes
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", (req, res) => {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", (req, res) => {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/tables", (req, res) => {
+  return res.json(tables);
+});
+
+app.get("/api/waitlist", (req, res) => {
+  return res.json(waitlist);
+});
+
+app.get("/tables#", (req, res) => {
+  //clear tables and wait list
+});
+
+app.post("/api/tables", (req, res) => {
+  let newReservation = req.body;
+  if(tables.length < 5) {
+    tables.push(newReservation);
+    res.send("table booked");
+  }
+  else {
+    waitlist.push(newReservation);
+    res.send("waitlisted");
+  }
+});
+
+//start listening
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
